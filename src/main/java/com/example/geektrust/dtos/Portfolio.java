8d2be0c9360adae1447b693e4b5c2fb932c76bd9
Kanza Romman <kanzaromman96@gmail.com>
2023-07-01 23:03:30 +0500

    refactor: added allocatedPercentage & related functions in Portfolio

--- src/main/java/com/example/geektrust/dtos/Portfolio.java
+++ src/main/java/com/example/geektrust/dtos/Portfolio.java
@@ -8,10 +8,13 @@ import java.util.Map;
 public class Portfolio {
     private final Map<Integer, Investment> portfolios;
     private final List<Double> systematicInvestmentPlan;
+    private final List<Double> allocatedPercentage;
+    private static final int INITIAL_MONTH = 0;
 
     public Portfolio() {
         portfolios = new HashMap<>();
         systematicInvestmentPlan = new ArrayList<>();
+        allocatedPercentage = new ArrayList<>();
     }
 
     public void addToPortfolio(int month, Investment investment) {
@@ -32,5 +35,17 @@ public class Portfolio {
 
     public Double getSystematicInvestmentPlanAmount(int index) {
         return systematicInvestmentPlan.get(index);
+    }
+
+    public void setAllocatedPercentage() {
+        Investment initialInvestment = getInvestmentByMonth(INITIAL_MONTH);
+        for (int investmentIndex = 0; investmentIndex < initialInvestment.getInvestmentCount(); investmentIndex++) {
+            double investmentPercentage = initialInvestment.getInvestmentPercentage(investmentIndex);
+            allocatedPercentage.add(investmentIndex, investmentPercentage);
+        }
+    }
+
+    public List<Double> getAllocatedPercentage() {
+        return this.allocatedPercentage;
     }
 }
